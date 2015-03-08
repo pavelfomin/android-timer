41e7cc8d13d36441351a23722ae7caba66526451
Pavel Fomin <p.v.fomin@gmail.com>
2015-03-07 20:09:50 -0600

    0.0.17: Added leading 0s to the minutes format. Fixes #7.

--- src/com/pvlf/android/timer/model/Lap.java
+++ src/com/pvlf/android/timer/model/Lap.java
@@ -109,7 +109,7 @@ public class Lap implements Serializable {
         int hours = minutes / 60;
         minutes = minutes % 60;
 
-        return String.format("%d:%d:%02d:%02d", hours, minutes, seconds, (millis % 1000) / 10);
+        return String.format("%d:%02d:%02d:%02d", hours, minutes, seconds, (millis % 1000) / 10);
     }
 	
 	/**
